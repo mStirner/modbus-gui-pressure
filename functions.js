608d9b9f8c0f696ba9bbc1faa494407913172635
Marc Stirner <marc.stirner@hotmail.de>
2024-11-19 08:47:59 +0100

    code cleanup & upgrade event listener added

---
+++ functions.js
@@ -0,0 +1,81 @@
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+function toFixedTruncate(number, decimals) {
+    const factor = Math.pow(10, decimals);
+    return Math.floor(number * factor) / factor;
+}
+
+
+function crc16Modbus(buffer) {
+    let crc = 0xFFFF;
+
+    for (let pos = 0; pos < buffer.length; pos++) {
+        crc ^= buffer[pos]; // XOR byte into least significant byte of crc
+
+        for (let i = 8; i !== 0; i--) { // Loop over each bit
+            if ((crc & 0x0001) !== 0) { // If the LSB is set
+                crc >>= 1;              // Shift right
+                crc ^= 0xA001;          // Apply XOR with polynomial
+            } else {
+                crc >>= 1;              // Just shift right
+            }
+        }
+    }
+
+    // CRC is usually appended in little-endian, so swap the bytes
+    return Buffer.from([crc & 0xFF, (crc >> 8) & 0xFF]);
+}
+
+
+function createModbusMessage(id, register) {
+
+    let buff = Buffer.from([
+        id,         // device id
+        0x04,       // function code (Read Input Register)
+        0x00,       // register address placeholder byte 1
+        0x00,       // register address placeholder byte  2
+        0x00,       // num of coils byte 1
+        0x02        // num of coils byte 2
+    ]);
+
+    buff.writeUInt8(id, 0); // replace device id
+    buff.writeUInt16BE(register, 2); // replace register
+
+    //console.log("Modbus message", buff);
+
+    return buff;
+
+}
+
+
+function parseModbusMessage(request, data) {
+
+    let resp = data.slice(0, data.length - 2);
+    let crc = data.slice(data.length - 2);
+    let payload = resp.slice(3, data.length - 2);
+
+    if (Buffer.compare(crc, crc16Modbus(resp)) !== 0) {
+        throw new Error("Invalid checksum");
+    }
+
+    if (request[0] !== data[0]) {
+        throw new Error("Invalid device Id");
+    }
+
+    return payload.readFloatBE();
+
+}
+
+
+module.exports = {
+    getRandomInt,
+    toFixedTruncate,
+    crc16Modbus,
+    createModbusMessage,
+    parseModbusMessage
+};
--- index.js
+++ index.js
@@ -1,35 +1,50 @@
 
-const { createServer } = require("http");
+const { createServer, ServerResponse } = require("http");
 const express = require("express");
 const { WebSocket } = require("ws");
 const { createInterface } = require("readline");
 const { SerialPort } = require("serialport");
 const timers = require("timers/promises");
+const {
+    getRandomInt,
+    toFixedTruncate,
+    crc16Modbus,
+    createModbusMessage,
+    parseModbusMessage
+} = require("./functions.js");
 
 const app = express();
 const server = createServer(app);
 var port = null;
 
+// handle websocket upgrade properly
+// otherwise the req.socket get closed
+// and the ws connection is dropped
+server.on("upgrade", (req, socket) => {
+
+    let res = new ServerResponse(req);
+    res.assignSocket(socket);
+
+    res.on("finish", () => {
+        res.socket.destroy();
+    });
+
+    app(req, res);
+
+});
+
+
+// server static files from folder public
 app.use(express.static('public'));
 
+
+// init websocket server
 const wss = new WebSocket.Server({
     noServer: true
 });
 
 
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-
-function toFixedTruncate(number, decimals) {
-    const factor = Math.pow(10, decimals);
-    return Math.floor(number * factor) / factor;
-}
-
-
+// handle serial port open
 function openSerialPort(opts) {
     port = new SerialPort({
         path: "/dev/ttyUSB0",   //initialise USB adapter
@@ -43,6 +58,7 @@ function openSerialPort(opts) {
 }
 
 
+// handle serial port close
 function closeSerialPort() {
     if (port && port.isOpen) {
         port?.close();
@@ -50,67 +66,7 @@ function closeSerialPort() {
 }
 
 
-function crc16Modbus(buffer) {
-    let crc = 0xFFFF;
-
-    for (let pos = 0; pos < buffer.length; pos++) {
-        crc ^= buffer[pos]; // XOR byte into least significant byte of crc
-
-        for (let i = 8; i !== 0; i--) { // Loop over each bit
-            if ((crc & 0x0001) !== 0) { // If the LSB is set
-                crc >>= 1;              // Shift right
-                crc ^= 0xA001;          // Apply XOR with polynomial
-            } else {
-                crc >>= 1;              // Just shift right
-            }
-        }
-    }
-
-    // CRC is usually appended in little-endian, so swap the bytes
-    return Buffer.from([crc & 0xFF, (crc >> 8) & 0xFF]);
-}
-
-
-function createModbusMessage(id, register) {
-
-    let buff = Buffer.from([
-        id,         // device id
-        0x04,       // function code (Read Input Register)
-        0x00,       // register address placeholder byte 1
-        0x00,       // register address placeholder byte  2
-        0x00,       // num of coils byte 1
-        0x02        // num of coils byte 2
-    ]);
-
-    buff.writeUInt8(id, 0); // replace device id
-    buff.writeUInt16BE(register, 2); // replace register
-
-    //console.log("Modbus message", buff);
-
-    return buff;
-
-}
-
-
-function parseModbusMessage(request, data) {
-
-    let resp = data.slice(0, data.length - 2);
-    let crc = data.slice(data.length - 2);
-    let payload = resp.slice(3, data.length - 2);
-
-    if (Buffer.compare(crc, crc16Modbus(resp)) !== 0) {
-        throw new Error("Invalid checksum");
-    }
-
-    if (request[0] !== data[0]) {
-        throw new Error("Invalid device Id");
-    }
-
-    return payload.readFloatBE();
-
-}
-
-
+// handle modbus query, wrapper function
 function handleModbus(id, register) {
     return new Promise((resolve, reject) => {
 
@@ -122,6 +78,8 @@ function handleModbus(id, register) {
             request,
             crc16Modbus(request)
         ]);
+
+        console.log();
 
         // write data to serial port
         // wait till its flushed
@@ -167,9 +125,9 @@ function handleModbus(id, register) {
                 // remove message handler after 1.5s
                 // when the device does not respond
                 timeout = setTimeout(() => {
-                    console.log("No message received, remove handler, prevents memeory leak")
+                    console.log("No message received for request, remove handler, prevents memeory leak", payload)
                     port.off("data", messageHandler);
-                }, 1000);
+                }, 1500);
 
                 // flushed payload data
                 // listen for response data
@@ -222,14 +180,9 @@ wss.on("connection", (ws, req) => {
 
     let interval = null;
 
-    ws.isAlive = true;
-    ws.on('error', console.error);
-    ws.on('pong', () => {
-        ws.isAlive = true;
-    });
-
     ws.once("close", () => {
         console.log("ws connection closed");
+        closeSerialPort();
         clearInterval(interval);
     });
 
@@ -371,18 +324,12 @@ wss.on("connection", (ws, req) => {
 
 });
 
+
 wss.on('close', function close() {
+    closeSerialPort();
     clearInterval(interval);
 });
 
-const interval = setInterval(function ping() {
-    wss.clients.forEach(function each(ws) {
-        if (ws.isAlive === false) return ws.terminate();
-
-        ws.isAlive = false;
-        ws.ping();
-    });
-}, 5000);
 
 app.get("/events", (req, res) => {
     if (req.headers["upgrade"] && req.headers["connection"]) {
@@ -400,7 +347,6 @@ app.get("/events", (req, res) => {
 
     }
 });
-
 
 
 server.listen(8123, "0.0.0.0", () => {
